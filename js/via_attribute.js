a55746e4ee80d02a5cb465116d8e58334cbe0868
ioretcio <Goncharukyou@gmail.com>
2021-04-27 16:26:28 +0300

    Local6

--- js/via_attribute.js
+++ js/via_attribute.js
@@ -9,18 +9,18 @@
 
 'use strict'
 
-const _VIA_ATTRIBUTE_TYPE = { 'TEXT':1, 'CHECKBOX':2, 'RADIO':3, 'SELECT':4, 'IMAGE':5 };
+const _VIA_ATTRIBUTE_TYPE = { 'TEXT': 1, 'CHECKBOX': 2, 'RADIO': 3, 'SELECT': 4, 'IMAGE': 5 };
 
 const _VIA_ATTRIBUTE_ANCHOR = {
-  'FILE1_Z0_XY1':'Spatial Region in an Image (e.g. bounding box of an object)',
+  'FILE1_Z0_XY1': "Прив'язка до прямокутника",
 };
 
 function _via_attribute(name, anchor_id, type, desc, options, default_option_id) {
-  this.aname     = name;
+  this.aname = name;
   this.anchor_id = anchor_id;
-  this.type      = type;
-  this.desc      = desc;
-  this.options   = options;
+  this.type = type;
+  this.desc = desc;
+  this.options = options;
   this.default_option_id = default_option_id;
 }
 
